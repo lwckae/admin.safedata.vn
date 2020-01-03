40afb8bbf9e6b04999cfec9a463e0138426bee57
unknown <lwckae@gmail.com>
2020-01-03 14:16:30 +0700

    update shell remote

--- application/controllers/Server.php
+++ application/controllers/Server.php
@@ -4,12 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Server extends CI_Controller {
     public function checkConnection()
     {
+        max_execution_time(10);
+        max_input_time(10);
         $server = array(
             'username' => $this->input->post('username'),
             'hostname' => $this->input->post('hostname'),
             'port' => $this->input->post('port')
         );
-        $data = system('ssh '.$server['username'].'@'.$server['hostname'].' -p '.$server['port'].' -o '.'StrictHostKeyChecking=no echo test');
+        $data = shell_exec('ssh '.$server['username'].'@'.$server['hostname'].' -p '.$server['port'].' -o '.'StrictHostKeyChecking=no echo test');
         if($data == 'test')
         {
             echo '<div class="badge badge-success"><i class="fa fa-check"></i> Successfully</div>';
