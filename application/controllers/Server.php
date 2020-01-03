fdd970a7cab861fe760d7440fbd7a1042d471864
unknown <lwckae@gmail.com>
2020-01-03 14:25:25 +0700

    update not supported for windows server

--- application/controllers/Server.php
+++ application/controllers/Server.php
@@ -4,20 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Server extends CI_Controller {
     public function checkConnection()
     {
-        max_execution_time(10);
-        max_input_time(10);
         $server = array(
             'username' => $this->input->post('username'),
             'hostname' => $this->input->post('hostname'),
             'port' => $this->input->post('port')
         );
-        $data = shell_exec('ssh '.$server['username'].'@'.$server['hostname'].' -p '.$server['port'].' -o '.'StrictHostKeyChecking=no echo test');
-        if($data == 'test')
+        if(passthru('uname') == 'Linux')
         {
-            echo '<div class="badge badge-success"><i class="fa fa-check"></i> Successfully</div>';
+            $data = shell_exec('ssh '.$server['username'].'@'.$server['hostname'].' -p '.$server['port'].' -o '.'StrictHostKeyChecking=no echo test');
+            if($data == 'test')
+            {
+                echo '<div class="badge badge-success"><i class="fa fa-check"></i> Successfully</div>';
+            }
+            else
+                echo '<div class="badge badge-danger">Failed</div>';
         }
         else
-            echo '<div class="badge badge-danger">Failed</div>';
+        {
+            echo '<div class="badge badge-warning">Not supported!</div>';
+        }
+        
     }
 }
 
